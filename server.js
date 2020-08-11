1e6768e12e575bd6907bab11b4a49afc9c7d9bff
MoKi <61506669+moki929@users.noreply.github.com>
2020-08-11 19:18:26 -0400

    heroku start

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const db = knex({
 
 const app = express();
 
-console.log(image)
+console.log(process.env)
 
 app.use(bodyParser.json());
 app.use(cors())
@@ -33,8 +33,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log('app is running on port ${process.env.PORT}');
 })
 
 
