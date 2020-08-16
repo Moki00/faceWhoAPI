9f446488a0bec8e1a22fe6302a178c788e9bdf3f
MoKi <61506669+moki929@users.noreply.github.com>
2020-08-16 14:18:37 -0400

    server update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.database_url,
+    connectionString : process.env.DATABASE_URL,
     ssl: true,
   }
 });
